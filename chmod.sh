76259ba6881f5e2b0cbb4d9662abf8686a5e5864
Helaine Burger <110602062+helaineburger@users.noreply.github.com>
2023-02-10 23:33:29 +0800

    Add files via upload

---
+++ chmod.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+cd /home/user/Documents/Research/Final/Basic-Antenna-Trainer-Kit-main
+sudo chmod +x about.pyw
+sudo chmod +x antenna_config_gui.pyw
+sudo chmod +x calc_offset.py
+sudo chmod +x calibrate_gui.pyw
+sudo chmod +x contact.pyw
+sudo chmod +x error_prompt.pyw
+sudo chmod +x fm_radio.py
+sudo chmod +x lab_menu_gui.pyw
+sudo chmod +x launcher.sh
+sudo chmod +x login.pyw
+sudo chmod +x main_gui.pyw
+sudo chmod +x HyFlex.desktop
+sudo chmod +x test.sh
+cd /home/user/Documents/Research/Final/Basic-Antenna-Trainer-Kit-main/Scripts
+sudo chmod +x anim_plot_online.pyw
+sudo chmod +x config_functions.py
+sudo chmod +x send_data_online.py
+sudo chmod +x servo_listener.py
+sudo chmod +x static_plot_online.pyw
+echo "done"
+$SHELL
+
