42e026b833fcc1da7f7770c1b6780be41ea00378
Helaine Burger <110602062+helaineburger@users.noreply.github.com>
2023-02-19 21:27:14 +0800

    Update servo_listener.py

--- Scripts/servo_listener.py
+++ Scripts/servo_listener.py
@@ -1,9 +1,17 @@
+####################
+## Import Modules ##
+####################
+
 import RPi.GPIO as GPIO
 import mysql.connector
 from time import sleep
 import config_functions as cf
 
-def x_axis(angle):
+#####################################################
+## Listen For Servo Axis Data Inputs From Database ##
+#####################################################
+
+def theta_axis(angle):
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(7, GPIO.OUT)
 
@@ -18,7 +26,7 @@ def x_axis(angle):
     pwm.stop()
     GPIO.cleanup()
     
-def y_axis(angle):
+def phi_axis(angle):
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(13, GPIO.OUT)
 
@@ -34,10 +42,10 @@ def y_axis(angle):
     GPIO.cleanup()
 
 while True:
-    curr_ax = cf.servo_x_y_get()
-    x_axis_angle = curr_ax[0]
-    # print(x_axis_angle)
-    y_axis_angle = curr_ax[1]
-    # print(y_axis_angle)
-    x_axis(x_axis_angle)
-    y_axis(y_axis_angle)
+    curr_ax = cf.servo_theta_phi_get() # get current axis data from database
+    theta_axis_angle = curr_ax[0]
+    # print(theta_axis_angle)
+    phi_axis_angle = curr_ax[1] 
+    # print(phi_axis_angle)
+    theta_axis(theta_axis_angle) # set servo theta angle to value from database
+    phi_axis(phi_axis_angle) # set servo phi angle to value from database
